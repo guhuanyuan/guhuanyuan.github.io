276d9d78ed3b0d3db01233fea57a4114ff570175
guhuanyuan <47204511+guhuanyuan@users.noreply.github.com>
2019-02-03 22:03:03 +0800

    Site updated: 2019-02-03 22:02:34

---
+++ dist/music.js
@@ -0,0 +1,46 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "借我",
+        artist: '谢春花',
+        url: 'http://m10.music.126.net/20190203220149/a4f70c35ca26ccc08b36216b56ac7bbd/ymusic/6f52/5c4e/f802/afac355d40a1834ca87638eeef6579ce.mp3',
+        cover: 'http://pic.sogou.com/d?query=%D0%BB%B4%BA%BB%A8+%BD%E8%CE%D2&mode=1&did=1',
+      },
+      {
+        name: "我一定会爱上你",
+        artist: '谢春花',
+        url: 'http://m10.music.126.net/20190203220149/9cf04a0d40712b468967a4de463cb29f/ymusic/c1cf/dfec/a0a3/52fbde573b26e129695c12301a6569ca.mp3',
+        cover: 'http://pic.sogou.com/d?query=%D0%BB%B4%BA%BB%A8+%BD%E8%CE%D2&mode=1&did=1',
+      },
+      {
+        name: '只道寻常',
+        artist: '谢春花',
+        url: 'http://m10.music.126.net/20190203220149/a8944ffa09d734fe7c94dd1a7d9d8719/ymusic/67f9/c5b8/c683/68709eb172bb65403b98972874f58736.mp3',
+        cover: 'http://pic.sogou.com/d?query=%D0%BB%B4%BA%BB%A8+%BD%E8%CE%D2&mode=1&did=1',
+      },
+      {
+        name: '理想三旬 ',
+        artist: '谢春花',
+        url: 'http://m10.music.126.net/20190203220149/bbebcd60913425c835828d0036809caf/ymusic/017f/12ec/9506/af7c5bbec6d86ac8fd020b4799349483.mp3',
+        cover: 'http://pic.sogou.com/d?query=%D0%BB%B4%BA%BB%A8+%C0%ED%CF%EB%C8%FD%D1%AE&mode=1&did=25',
+      }
+      {
+        name: '还想听你的故事',
+        artist: '谢春花',
+        url: 'http://m10.music.126.net/20190203220149/dff3894c27d649324a743a35863939f2/ymusic/d866/e5e3/5b6b/97afce6a4b7a35885bedd4c5aaf70236.mp3',
+        cover: 'http://pic.sogou.com/d?query=%D0%BB%B4%BA%BB%A8+%BD%E8%CE%D2&mode=1&did=1',
+      }
+      {
+        name: '这个大叔不太冷',
+        artist: '花粥，马良',
+        url: 'http://m10.music.126.net/20190203221453/8494d935e57f59c4099d6bdc3880298f/ymusic/8b86/84d4/efec/5a7bbf0a13100f393b3add6e67c6052c.mp3',
+        cover: 'http://pic.sogou.com/d?query=%BB%A8%D6%E0+%D5%E2%B8%F6%B4%F3%CA%E5&mode=1&did=16',
+      }
+
+
+
+    ]
+});
